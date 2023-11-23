d1c9397ca15c98a14d6fc78903e1e2154e168fd9
Катя <kat699840@gmail.com>
2023-11-23 11:03:44 +0300

    task4 is done

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -15,3 +15,24 @@ function fetchMeetupById(meetupId) {
 }
 
 // Требуется создать Vue приложение
+const App = defineComponent({
+  name: 'App',
+
+  data() {
+    return {
+      selectedValue: '',
+      title: null,
+      meetupId: null
+    }
+  },
+  watch: {
+    selectedValue: function(meetupId) {
+      fetchMeetupById(meetupId).then(data => {
+        this.title = data.title
+      })
+    }
+  }
+})
+
+const app = createApp(App);
+const vm = app.mount('#app'); 
