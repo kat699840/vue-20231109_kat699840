f597ddd9a68b3e22bbee6cd63769ea6826028292
Катя <kat699840@gmail.com>
2023-11-24 12:02:12 +0300

    3rd task done

--- 02-components/03-MeetupCover/components/MeetupCover.js
+++ 02-components/03-MeetupCover/components/MeetupCover.js
@@ -2,11 +2,19 @@ import { defineComponent } from '../vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupCover',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    image: {
+      type: String,
+      default: ''
+    }
+  },
 
   template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
-        <!-- Ссылка выше - пример верстки при наличии изображения, а не изображение по умолчанию. -->
-        <!-- Изображение по умолчанию уже есть в стилях. Для его вывода достаточно не добавлять никаких стилей. -->
-        <h1 class="meetup-cover__title">Title</h1>
-    </div>`,
+    <div class="meetup-cover" :style="{ '--bg-url': image ? 'url(' + image + ')' : '' }">
+        <h1 class="meetup-cover__title">{{ title }}</h1>
+    </div>`
 });
