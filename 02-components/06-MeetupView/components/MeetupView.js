23fd61f9a50879219094a1909d999e26aedaf268
Катя <kat699840@gmail.com>
2023-11-28 21:50:03 +0300

    task6 done

--- 02-components/06-MeetupView/components/MeetupView.js
+++ 02-components/06-MeetupView/components/MeetupView.js
@@ -1,6 +1,11 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
 import UiAlert from './UiAlert.js';
 import UiContainer from './UiContainer.js';
+import MeetupDescription from '../../02-MeetupDescription/components/MeetupDescription.js';
+import MeetupCover from '../../03-MeetupCover/components/MeetupCover.js';
+import MeetupInfo from '../../04-MeetupInfo/components/MeetupInfo.js';
+import MeetupAgendaItem from '../../05-MeetupAgenda/components/MeetupAgendaItem.js';
+import MeetupAgenda from '../../05-MeetupAgenda/components/MeetupAgenda.js';
 
 export default defineComponent({
   name: 'MeetupView',
@@ -8,6 +13,11 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupDescription,
+    MeetupCover,
+    MeetupInfo,
+    MeetupAgendaItem,
+    MeetupAgenda
   },
 
   props: {
@@ -17,22 +27,36 @@ export default defineComponent({
     },
   },
 
+methods: {
+  check_agenda() {
+    
+    if (this.meetup.agenda == undefined || this.meetup.agenda == null || this.meetup.agenda.length == 0) {
+      return false 
+    }
+    
+    return true
+  }
+},
+
   template: `
     <div>
-      <!-- meetup cover-->
-
+      <MeetupCover :title="this.meetup.title" :image="this.meetup.image"/>
       <UiContainer>
         <div class="meetup">
           <div class="meetup__content">
             <h3>Описание</h3>
-            <!-- meetup description -->
 
+            <MeetupDescription :description="this.meetup.description"/>
             <h3>Программа</h3>
-            <!-- meetup agenda -->
-            <UiAlert>Программа пока пуста...</UiAlert>
+            <div v-if="check_agenda()">
+              <MeetupAgenda :agenda="this.meetup.agenda" />
+            </div>
+            <div v-else>
+              <UiAlert>Программа пока пуста…</UiAlert>
+            </div>
           </div>
           <div class="meetup__aside">
-            <!-- meetup info -->
+            <MeetupInfo :organizer="this.meetup.organizer" :place="this.meetup.place" :date="this.meetup.date" />
           </div>
         </div>
       </UiContainer>
